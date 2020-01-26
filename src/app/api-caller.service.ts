9620bdcdbc27e8bdcb488882b35cc18e5be53507
Chien  Tan <chien.tan16@imperial.ac.uk>
2020-01-26 18:45:02 +0000

    try

--- src/app/api-caller.service.ts
+++ src/app/api-caller.service.ts
@@ -1,21 +1,18 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { NewsItem } from 'src/data/NewsItem';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { NewsItem } from "src/data/NewsItem";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ApiCallerService {
-  private serviceUrl: string = 'http://127.0.0.1:5000';
-  
-  constructor(private http: HttpClient) { }
+  // private serviceUrl: string = 'http://127.0.0.1:5000';
+  private serviceUrl: string = "https://indiv-proj-serv.herokuapp.com";
+
+  constructor(private http: HttpClient) {}
 
   public getNewsItemsRaw(): Observable<NewsItem[]> {
-    return this.http.get<NewsItem[]>(this.serviceUrl + '/news-items');
+    return this.http.get<NewsItem[]>(this.serviceUrl + "/news-items");
   }
-
-
-
-
 }
